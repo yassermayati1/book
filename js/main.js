dc5d1172e249ac1927ad5aafe5ac5f605850b51f
yassermayati1 <yasser.mayati1@gmail.com>
2022-05-08 20:53:31 +0300

    add new paget

--- js/main.js
+++ js/main.js
@@ -197,23 +197,7 @@ $(document).ready(function(){
 		i.style.left = n.clientX + "px", 
 		i.style.top = n.clientY + "px"
     });
-    var t = document.getElementById("cursor"),
-        e = document.getElementById("cursor2"),
-        i = document.getElementById("cursor3");
-    function n(t) {
-        e.classList.add("hover"), i.classList.add("hover")
-    }
-    function s(t) {
-        e.classList.remove("hover"), i.classList.remove("hover")
-    }
-    s();
-    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-        o(r[a])
-    }
-    function o(t) {
-        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-    }
-	
+    
 	var pos = 0;
 			window.setInterval(function(){
 				pos++;
